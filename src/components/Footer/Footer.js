579c314af56cf67fbd44679e2dc1f8706b5a1260
Charizard17 <3723esad@gmail.com>
2023-03-17 15:25:01 +0100

    acomplishments and footer components finished

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,45 @@
-import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import React from "react"
+import {AiFillGithub, AiFillInstagram, AiFillLinkedin} from "react-icons/ai"
 
-import { SocialIcons } from '../Header/HeaderStyles';
-import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+import {SocialIcons} from "../Header/HeaderStyles"
+import {
+  CompanyContainer,
+  FooterWrapper,
+  LinkColumn,
+  LinkItem,
+  LinkList,
+  LinkTitle,
+  Slogan,
+  SocialContainer,
+  SocialIconsContainer,
+} from "./FooterStyles"
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
-  );
-};
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel:123-456-7890">123-456-7890</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mail:test@mail.com">test@mail.com</LinkItem>
+        </LinkColumn>
+        <SocialIconsContainer>
+          <Slogan>Innovating one project at a time</Slogan>
+        </SocialIconsContainer>
+        <SocialContainer>
+          <SocialIcons href="https://github.com">
+            <AiFillGithub size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="https://linkedin.com">
+            <AiFillLinkedin size="3rem" />
+          </SocialIcons>
+        </SocialContainer>
+      </LinkList>
+    </FooterWrapper>
+  )
+}
 
-export default Footer;
+export default Footer
