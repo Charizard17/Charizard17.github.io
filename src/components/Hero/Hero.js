3bac7f6bc63b3caff1979e271f2dfa99488fc7f9
Charizard17 <3723esad@gmail.com>
2023-03-17 12:25:56 +0100

    webpack errors resolved, next version edited, some components edited, gitignore edited

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React from "react"
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {Section, SectionText, SectionTitle} from "../../styles/GlobalComponents"
+import Button from "../../styles/GlobalComponents/Button"
+import {LeftSection} from "./HeroStyles"
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
-);
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem aliquid
+        aspernatur ex eius ipsa ducimus enim ullam? Et quo cupiditate
+        repudiandae, vel in ullam consequuntur voluptas iste corporis, nihil
+        porro.
+      </SectionText>
+      <Button onClick={() => (window.location = "https://google.com")}>
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
+)
 
-export default Hero;
+export default Hero
